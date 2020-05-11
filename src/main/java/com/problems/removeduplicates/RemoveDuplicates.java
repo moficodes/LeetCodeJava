c7293da48c2c8e8f6d172c0575499527c7af68ed
Mofizur Rahman <moficodes@gmail.com>
2020-05-11 02:26:59 -0400

    clean slate

--- src/main/java/com/problems/removeduplicates/RemoveDuplicates.java
+++ src/main/java/com/problems/removeduplicates/RemoveDuplicates.java
@@ -2,25 +2,7 @@ package com.problems.removeduplicates;
 
 public class RemoveDuplicates {
   public int removeduplicates(int[] input){
-    
-    if(input.length == 0 || input.length ==1)
-      return input.length;
-
-    int j =0;
-
-    // [0,1,2,0,1,1,1,1,2,2,2]
-    for(int i=0; i< input.length -1; i++){
-      if(input[i] != input[i+1]){
-        input[j] = input[i];
-        j++;
-      }
-    }
-
-    input[j] = input[input.length -1];
-    return j+1;
+    //TODO: implement me
+    return 0;
   }
 }
-
-
-// return the size of the unique element
-//  create a new array with unique elements
